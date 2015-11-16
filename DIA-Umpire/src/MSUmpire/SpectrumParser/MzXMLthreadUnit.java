7816bc5991ac20e51f257010b700949c01b85b13
Chih-Chiang Tsou <tsouc@umich.edu>
2015-11-16 11:47:43 -0500

    updated

--- DIA-Umpire/src/MSUmpire/SpectrumParser/MzXMLthreadUnit.java
+++ DIA-Umpire/src/MSUmpire/SpectrumParser/MzXMLthreadUnit.java
@@ -22,8 +22,6 @@ package MSUmpire.SpectrumParser;
 import MSUmpire.BaseDataStructure.InstrumentParameter;
 import MSUmpire.BaseDataStructure.ScanData;
 import MSUmpire.BaseDataStructure.SpectralDataType;
-import java.awt.Color;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -31,13 +29,6 @@ import java.util.zip.DataFormatException;
 import javax.xml.parsers.ParserConfigurationException;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.statistics.HistogramDataset;
-import org.jfree.data.statistics.HistogramType;
 import org.xml.sax.SAXException;
 
 /**
@@ -64,28 +55,6 @@ public class MzXMLthreadUnit implements Runnable {
         this.XMLtext = XMLtext;
         this.parameter = parameter;
         this.dataType = dataType;
-    }
-
-    private void DrawIntDis() {
-        double[] PeakInt = new double[scan.Data.size()];
-        for (int i = 0; i < scan.Data.size(); i++) {
-            PeakInt[i] = scan.Data.get(i).getY();
-        }
-        HistogramDataset histogramDataset = new HistogramDataset();
-        histogramDataset.setType(HistogramType.RELATIVE_FREQUENCY);
-        histogramDataset.addSeries("Intensity", PeakInt, 100);
-
-        JFreeChart chart = ChartFactory.createHistogram("Peak intensity distribution", "Intensity", "Frequency", histogramDataset, PlotOrientation.VERTICAL, true, false, false);
-        XYPlot plot = chart.getXYPlot();
-        plot.setBackgroundPaint(Color.white);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
-        plot.setForegroundAlpha(0.8f);
-        chart.setBackgroundPaint(Color.white);
-        try {
-            ChartUtilities.saveChartAsPNG(new File(scan.MsLevel + "_ " + scan.ScanNum + ".PNG"), chart, 1000, 600);
-        } catch (IOException e) {
-        }
     }
 
     private void Read() throws FileNotFoundException, IOException, ParserConfigurationException, SAXException, DataFormatException {
